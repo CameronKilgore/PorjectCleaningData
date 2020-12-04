4684f2cb3a6894080cb074fa9127c6bb91f86f86
CameronKilgore <cameronpkilgore@gmail.com>
2020-12-03 21:06:01 -0500

    Adds run_analysis, datasets, etc

---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+
+'Bring on the initial featurse and activity labels datasets'
+library(stringr)
+features<-read.table("./UCI HAR Dataset/features.txt")[,2]
+activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt",
+                            col.names = c("ActivityNumber","Activity"))
+'Clean up the activity names'
+activity_labels[,2]<-str_to_title(gsub("_", " ",activity_labels[,2]))
+
+'section should be either "Test" or "Train"'
+initial_clean<-function(section){
+  if(section != "Test" & section != "Train"){
+    print("Invalid section")
+    stop()
+  }
+  
+  if(section=="Test"){
+    X_temp<-read.table("./UCI HAR Dataset/test/X_test.txt")
+    Y_temp<-read.table("./UCI HAR Dataset/test/Y_test.txt",
+                       col.names="ActivityNumber")
+    temp_subjects<-read.table("./UCI HAR Dataset/test/subject_test.txt",
+                              col.names="Subject")
+  }
+  else if(section=="Train"){
+      X_temp<-read.table("./UCI HAR Dataset/train/X_train.txt")
+      Y_temp<-read.table("./UCI HAR Dataset/train/Y_train.txt",
+                         col.names="ActivityNumber")
+      temp_subjects<-read.table("./UCI HAR Dataset/train/subject_train.txt",
+                                col.names="Subject")
+  }
+  names(X_temp)<-features
+  trimX<-X_temp[,grep("-mean\\(\\)|-std\\(\\)",names(X_temp))]
+  
+  activities<-paste(section,merge(Y_temp, activity_labels, 
+                    by.y="ActivityNumber")$Activity)
+  
+  trimX<-cbind(temp_subjects, activities,trimX)
+  names(trimX)<-gsub("-std\\(\\)", " Standard Deviation",
+                     gsub("-mean\\(\\)", " Mean", names(trimX)))
+  return(trimX)
+}
+
+test_set<-initial_clean("Test")
+train_set<-initial_clean("Train")
+tidy_set<-rbind(test_set,train_set)
+means<-aggregate(tidy_set[,3:ncol(tidy_set)],
+                 list(tidy_set$Subject,tidy_set$activities), FUN=mean)
+
+write.table(tidy_set,file="./tidy_dataset.txt")
+write.table(means,file="./means.txt")
